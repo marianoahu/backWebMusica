81714561b38aa87d8114df0cba434bcd964b607a
Mariano Ahumada <marianoahumadapretel@gmail.com>
2024-07-20 11:45:14 +0200

    added UserDto

---
+++ src/main/java/com/webmusical/springboot/backend/apirest/models/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.webmusical.springboot.backend.apirest.models.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+
+    private long id;
+    private String name;
+    private String lastName;
+    private Date createAt;
+    private String email;
+
+    // Getters y Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
