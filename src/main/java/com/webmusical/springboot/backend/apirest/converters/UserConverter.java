f9397fd8b7d2792b9ae323378ee07a409bbd3bd7
Mariano Ahumada <marianoahumadapretel@gmail.com>
2024-07-20 11:44:08 +0200

    adding UserConverter

---
+++ src/main/java/com/webmusical/springboot/backend/apirest/converters/UserConverter.java
@@ -0,0 +1,27 @@
+package com.webmusical.springboot.backend.apirest.converters;
+
+import com.webmusical.springboot.backend.apirest.models.entity.User;
+import com.webmusical.springboot.backend.apirest.models.dto.UserDTO;
+
+public class UserConverter {
+
+    public static UserDTO entityToDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setLastName(user.getLastName());
+        dto.setCreateAt(user.getCreateAt());
+        dto.setEmail(user.getEmail());
+        return dto;
+    }
+
+    public static User dtoToEntity(UserDTO dto) {
+        User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto.getName());
+        user.setLastName(dto.getLastName());
+        user.setCreateAt(dto.getCreateAt());
+        user.setEmail(dto.getEmail());
+        return user;
+    }
+}
