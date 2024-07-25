7b1e322ecc2e4ee3f3f83aba8ed8ba951cfc09ff
Mariano Ahumada <marianoahumadapretel@gmail.com>
2024-07-25 08:25:05 +0200

    Updated UserController

---
+++ src/main/java/com/webmusical/springboot/backend/apirest/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.webmusical.springboot.backend.apirest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.webmusical.springboot.backend.apirest.converters.UserConverter;
+import com.webmusical.springboot.backend.apirest.models.dto.UserDTO;
+import com.webmusical.springboot.backend.apirest.models.entity.User;
+import com.webmusical.springboot.backend.apirest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:4200") // URL de tu frontend Angular
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUsuario(@RequestBody UserDTO userDTO) {
+        UserDTO newUserDTO = userService.save(userDTO);
+        try { //testing if json arrives correctly to back // it does not
+            // Crear una instancia de ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            // Convertir el objeto a JSON
+            String json = objectMapper.writeValueAsString(userDTO);
+
+            // Imprimir el JSON en la consola
+            System.out.println(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return new ResponseEntity<>(newUserDTO, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsuarios() {
+        List<UserDTO> usuarios = userService.findAll();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+
+
+
+    // Otros métodos para listar, actualizar y eliminar usuarios
+}
+
